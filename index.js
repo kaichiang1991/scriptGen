2c323cf324c74d9984e2bfea262c2887b6511190
Kai <addie60111@gmail.com>
2020-11-21 11:25:07 +0800

    - 新增資料夾遍歷功能 - 新增 input/ output 資料夾

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const DataParse = require('./dataParse.js')
 const NormalObject = require('./normalObj')
 const CallScriptObject = require('./callScriptObj.js')
 
-let workbook = XLSX.read('test2.xlsx', {type: 'file'})
-let sheet = workbook.Sheets['腳本設定']
-let jsonSheet = XLSX.utils.sheet_to_json(sheet, {header: 1, raw: true})
+let files = fs.readdirSync('./input')
+files.forEach(file =>{
+    let workbook = XLSX.read('./input/' + file, {type: 'file'})
+    let sheet = workbook.Sheets['腳本設定']
+    let jsonSheet = XLSX.utils.sheet_to_json(sheet, {header: 1, raw: true})
+    
+    let obj1 = new NormalObject(workbook, 'Script3')
+    let obj2 = new CallScriptObject(workbook, 'Script2')
+    
+    let output = {
+        Probability: DataParse.getProb(jsonSheet, '權重'),
+        NextScript: DataParse.getNextScript(jsonSheet, 'Next'),
+        Objects: [obj1, obj2]
+    }
+    
+    let outputName = DataParse.getOutputName(jsonSheet, '輸出檔名')
+    fs.writeFileSync('./output/' + outputName, JSON.stringify(output, null, 2))
+})
 
-let obj1 = new NormalObject(workbook, 'Script3')
-let obj2 = new CallScriptObject(workbook, 'Script2')
-
-let output = {
-    Probability: DataParse.getProb(jsonSheet, '權重'),
-    NextScript: DataParse.getNextScript(jsonSheet, 'Next'),
-    Objects: [obj1, obj2]
-}
-
-let outputName = DataParse.getOutputName(jsonSheet, '輸出檔名')
-console.log(outputName)
-fs.writeFileSync(outputName, JSON.stringify(output))
